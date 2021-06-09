c50e4865b13ca96ef96ee798501f0477ce16fba7
Igor Wysocki <narghar@gmail.com>
2021-06-09 10:37:40 +0200

    Modify OurWork section

--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 const OurWork = () => {
   return (
-    <section className="grid grid-cols-3 max-w-base mx-auto mt-32  px-2 xl:px-0 ">
+    <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 max-w-base mx-auto mt-32  px-2 xl:px-0 items-center justify-items-center md:justify-items-start">
       <div>
-        <p>1.</p>
-        <h2>Do dzisiaj wyprodukowaliśmy</h2>
-        <p>
+        <p className="font-bold text-orange text-3xl">01</p>
+        <h2 className="font-bold text-5xl mt-1">Do dzisiaj wyprodukowaliśmy</h2>
+        <p className="mt-14">
           ponad 600 odcinków animowanych seriali telewizyjnych o łącznej
           długości 5.600 minut oraz 7 pełnometrażowych filmów animowanych
         </p>
@@ -14,9 +14,9 @@ const OurWork = () => {
         <Image src="/assets/contact/01.png" alt="" width={427} height={526} />
       </div>
       <div>
-        <p>2.</p>
-        <h2>Dla producentów z krajów</h2>
-        <p>
+        <p className="font-bold text-orange text-3xl">02</p>
+        <h2 className="font-bold text-5xl mt-1">Dla producentów z krajów</h2>
+        <p className="mt-14">
           Belgia, Francja, Niemcy, Włochy, Kanada, Wielka Brytania, Hiszpania,
           Słowacja, Czechy, Indie, Iran, Luksemburg, Izrael i Polska.
         </p>
@@ -25,9 +25,9 @@ const OurWork = () => {
         <Image src="/assets/contact/02.png" alt="" width={434} height={349} />
       </div>
       <div>
-        <p>3.</p>
-        <h2>Liderzy naszych zespołów</h2>
-        <p>
+        <p className="font-bold text-orange text-3xl">03</p>
+        <h2 className="font-bold text-5xl mt-1">Liderzy naszych zespołów</h2>
+        <p className="mt-14">
           Belgia, Francja, Niemcy, Włochy, Kanada, Wielka Brytania, Hiszpania,
           Słowacja, Czechy, Indie, Iran, Luksemburg, Izrael i Polska.
         </p>
@@ -35,13 +35,13 @@ const OurWork = () => {
       <div>
         <Image src="/assets/contact/03.png" alt="" width={434} height={349} />
       </div>
-      <div className="col-span-2">
+      <div className="xl:col-span-2 order-last md:order-none">
         <Image src="/assets/contact/04.png" alt="" width={912} height={412} />
       </div>
       <div>
-        <p>4.</p>
-        <h2>Z reżyserami m.in.</h2>
-        <p>
+        <p className="font-bold text-orange text-3xl">04</p>
+        <h2 className="font-bold text-5xl mt-1">Z reżyserami m.in.</h2>
+        <p className="mt-14">
           Od Ariego Folmana, Kena Loacha, Romana Polańskiego, Wojtka
           Smarzowskiego i Witka Adamka
         </p>
