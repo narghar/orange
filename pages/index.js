814488affe9cafa504542d344241ca5665c0828a
Igor Wysocki <narghar@gmail.com>
2021-06-11 15:22:13 +0200

    Add link to Offer page

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,11 @@ export default function Home() {
     <nav className="h-screen max-w-screen-sm flex items-center justify-center bg-indigo-700 mx-auto">
       <ul className="text-white uppercase text-4xl leading-relaxed">
         <li>
+          <Link href="/oferta">
+            <a>Oferta</a>
+          </Link>
+        </li>
+        <li>
           <Link href="/kariera">
             <a>Kariera</a>
           </Link>
