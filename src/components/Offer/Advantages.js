3538874f536a4a0543611d13c9cbfb3c5d0b4b93
Robbo23 <47586046+Robbo23@users.noreply.github.com>
2021-06-11 18:01:21 +0200

    Update adventages.js - change id
    Nie musisz dla każdego obiektu w tablicy podawać "id", jeśli są to 1, 2, 3..., itd. Jeśli potrzebujesz id w zasadzie tylko dla "key" to możesz sobie robić tak jak Ci to tutaj poprawiłem.

--- src/components/Offer/Advantages.js
+++ src/components/Offer/Advantages.js
@@ -2,32 +2,26 @@ import AdvantageItem from "./AdvantageItem";
 
 const items = [
   {
-    id: 1,
     heading: "Lorem Ipsum",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
   },
   {
-    id: 2,
     heading: "Lorem Ipsum",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
   },
   {
-    id: 3,
     heading: "Lorem Ipsum",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
   },
   {
-    id: 4,
     heading: "Lorem Ipsum",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
   },
   {
-    id: 5,
     heading: "Lorem Ipsum",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
   },
   {
-    id: 6,
     heading: "Lorem Ipsum",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
   },
@@ -40,8 +34,8 @@ const Advantages = () => {
       <div className="max-w-base mx-auto pt-36 pb-48">
         <h2 className="text-5xl font-bold">Mocne strony</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-24 gap-x-20 gap-y-20">
-          {items.map( item => (
-            <AdvantageItem key={item.id} heading={item.heading} text={item.text} />
+          {items.map((item, i) => (
+            <AdvantageItem key={i} heading={item.heading} text={item.text} />
           ))}
         </div>
       </div>
